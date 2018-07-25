2e10089197d36b2128c859b634280371040448cf
Nate Thom <nathanthom@nevada.unr.edu>
2018-07-25 15:32:35 -0700

    Finally gave up on AJAX for now. I will continue working on the basic functionality of the app and add AJAX/Auto updating later.

--- smartgrid-website/dash/urls.py
+++ smartgrid-website/dash/urls.py
@@ -6,24 +6,13 @@ app_name = 'dash'
 urlpatterns = [
     # ex: /dash/
     path('', views.dash, name='dash'),
-
-    path('dash/statistics/', views.mean_stat, name='mean'),
-
+    
     # ex: /dash/dash_statistics_mean/
     path('dash_statistics_mean/', views.dash_statistics_mean, name='dash_statistics_mean'),
 
-    # ex: still need to discover
+    # ex: /dash/mean_statistic/?metric=consumption&data=neighborhood
     re_path(r'^mean_statistic/$', views.mean_statistic, name='mean_statistic'),
 
     # ex: /dash/visualization/
     path('visualization/', views.visualization, name='visualization'),
-
-    # ex: /dash/statistics/
-    path('statistics/', views.statistics, name='statistics'),
-    # ex: /dash/statistics/?metric=consumption&data=neighborhood
-
-    # ex: /dash/comparisons/
-    path('comparisons/', views.comparisons, name='comparisons'),
-    # ex: /dash/comparisons/?data=neighborhood
-    re_path(r'^number_of_comparison/$', views.number_of_comparison),
 ]
--- smartgrid-website/dash/views.py
+++ smartgrid-website/dash/views.py
@@ -8,33 +8,28 @@ from django.db.models import Count, Q
 from .models import Region, Aggregator, Neighborhood, House, Readings, Appliance, Appliance_Type
 from .forms import MeanStatistic
 
-#test
-def mean_stat(request):
-    if request.method == 'GET':
-        form = MeanStatistic(request.GET)
-        if form.is_valid():
-            return HttpResponseRedirect('/dash/')
-
 # Create your views here.
 def dash(request):
     context = {}
     return render(request, 'dash/dash.html', context)
 
 def dash_statistics_mean(request):
-    context = {}
+    mean_items = 0
+    context = {'mean_items': mean_items}
     return render(request, 'dash/dash_statistics_mean.html', context)
 
 def mean_statistic(request):
     metric = request.GET['metric']
     data = request.GET['data']
     if((metric == "consumption" and data == "Readings")):
-        data = json.dumps({'mean_items': -1, 'metric': metric, 'data': data,})
-        return HttpResponse(data, content_type='application/json')
+        #template = loader.get_template('dash/dash_statistics_mean.html')
+        context = {'mean_items': -1, "metric": metric, "data": data,}
+        return render(request, 'dash/dash_statistics_mean.html', context)
 
     if((metric == "kWh" and data != "Readings") or (metric == "outdoor_temp" and data != "Readings")):
-        template = loader.get_template('dash/statistics.html')
+        #template = loader.get_template('dash/dash_statistics_mean.html')
         context = {'mean_items': -1, "metric": metric, "data": data,}
-        return render(request, 'dash/statistics.html', context)
+        return render(request, 'dash/dash_statistics_mean.html', context)
 
     if(metric == "consumption"):
         metric = data.lower()+"_"+metric
@@ -45,15 +40,9 @@ def mean_statistic(request):
         temp = eval("item"+"."+metric)
         sum_items = temp + sum_items
     mean_items = sum_items/len(list)
-    template = loader.get_template('dash/statistics.html')
+    #template = loader.get_template('dash/dash_statistics_mean.html')
     context = {'list': list, 'mean_items': mean_items, 'sum_items': sum_items, 'metric': metric, 'data': data,}
-    return render(request, 'dash/statistics.html', context)
-
-def statistics(request):
-    mean_items = 0
-    template = loader.get_template('dash/statistics.html')
-    context = {'mean_items': mean_items,}
-    return render(request, 'dash/statistics.html', context)
+    return render(request, 'dash/dash_statistics_mean.html', context)
 
 def visualization(request):
     house_dataset = House.objects \
@@ -67,22 +56,3 @@ def visualization(request):
         len_house_dataset.append(house)
     context = {'house_dataset': house_dataset, 'region_dataset': region_dataset, 'len_house_dataset': len_house_dataset,}
     return render(request, 'dash/visualization.html', context)
-
-def comparisons(request):
-    readings_list = Readings.objects.all()
-    num_readings = len(readings_list)
-    neighborhoods_greater_25 = Neighborhood.objects.filter(neighborhood_consumption__gt=25)
-    neighborhoods_less_25 = Neighborhood.objects.filter(neighborhood_consumption__lte=25)
-    num_neighborhoods_greater_25 = len(neighborhoods_greater_25)
-    num_neighborhoods_less_25 = len(neighborhoods_less_25)
-    template = loader.get_template('dash/comparisons.html')
-    context = {'num_readings': num_readings, 'num_neighborhoods_less_25': num_neighborhoods_less_25, 'num_neighborhoods_greater_25': num_neighborhoods_greater_25,}
-    return render(request, 'dash/comparisons.html', context)
-
-def number_of_comparison(request):
-    data = request.GET['data']
-    list = eval(data).objects.all()
-    number_of = len(list)
-    template = loader.get_template('dash/comparisons.html')
-    context = {'number_of': number_of, 'data': data,}
-    return render(request, 'dash/comparisons.html', context)
